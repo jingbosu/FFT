59e9e5596a3b40f01f41eb7f29beb689c24ef362
jingbosu <jingbosu2010@hotmail.com>
2015-11-18 18:06:18 +0100

    Calcul des coefficients complex est fait

--- src/Project_FFT/src/TwiddleFactor.cpp
+++ src/Project_FFT/src/TwiddleFactor.cpp
@@ -7,6 +7,7 @@
 
 #include "TwiddleFactor.h"
 
+
 TwiddleFactor::TwiddleFactor() {
 	// TODO Auto-generated constructor stub
 
@@ -16,3 +17,16 @@ TwiddleFactor::~TwiddleFactor() {
 	// TODO Auto-generated destructor stub
 }
 
+vector<complex<float>> TwiddleFactor:: CalculFactors(){
+	vector<complex<float>> v(512);
+	complex<float> c;
+	float r, theta_radians;
+	r = 1;
+
+	for(int i = 0; i< N/2; i++){
+		theta_radians = -2*PI*i/N;
+		c = polar(r, theta_radians);
+		v[i] = c;
+	}
+	return v;
+}
--- src/Project_FFT/src/TwiddleFactor.h
+++ src/Project_FFT/src/TwiddleFactor.h
@@ -8,12 +8,18 @@
 #ifndef TWIDDLEFACTOR_H_
 #define TWIDDLEFACTOR_H_
 #include <iostream>
+#include <vector>
+#include <complex>
 using namespace std;
+
+#define PI 3.14159
+#define N 1024
 
 class TwiddleFactor {
 public:
 	TwiddleFactor();
 	virtual ~TwiddleFactor();
+	vector<complex<float>> CalculFactors();
 };
 
 #endif /* TWIDDLEFACTOR_H_ */
