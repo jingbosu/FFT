e93d503eac4bb0c84065910f7f9013ce8a070b3b
jingbosu <jingbosu2010@hotmail.com>
2015-11-20 22:37:56 +0100

    je suis desole de push qqch qui ne fonctionne pas

--- src/Project_FFT/src/FFT.cpp
+++ src/Project_FFT/src/FFT.cpp
@@ -16,3 +16,58 @@ FFT::~FFT() {
 	// TODO Auto-generated destructor stub
 }
 
+vector<complex<float>> FFT::calculFFT(vector<float> data_in){
+	//calculer l'etage de papillon
+	int nb_etage = log2(N);
+
+	int decalage_ligne = 0;
+	int etage, j, p, k;
+	vector<complex<float>> data_final(N);
+
+	/////////////////////Calculer bit reverse/////////////
+	BitReverse bit;
+	vector<float> data_reversed;
+	data_reversed = bit.ReverseVector(data_in);
+
+	///////////////////Calculer les coefficients//////////
+	TwiddleFactor tw;
+	vector<complex<float>> vec_comp;
+	vec_comp = tw.CalculFactors();
+
+	for (etage = 1; etage<=nb_etage; etage++){
+		decalage_ligne = pow(2, etage-1);
+		for (j = 0; j<=decalage_ligne-1; j++){
+			p = pow(2, nb_etage-etage)*j;
+			for (k = j; j<=N-1; pow(2,etage)){
+				//Mais la relation entre data_in et data_out?
+//				data_final(k) = data_final(k) + data_final(k+decalage_ligne)*vec_comp(p);
+//				data_final(k+decalage_ligne) = data_final(k) - data_final(k+decalage_ligne)*vec_comp(p);
+			}
+		}
+	}
+	return data_final;
+}
+
+complex<float> multiplicationComplex(float f, complex<float> cp){
+	complex<float> result;
+	complex<float> temp;
+	temp = (complex<float>)f;
+	result = temp * cp;
+	return result;
+}
+
+complex<float> additionComplex(float f, complex<float> cp){
+	complex<float> result;
+	complex<float> temp;
+	temp = (complex<float>)f;
+	result = temp + cp;
+	return result;
+}
+
+complex<float> soustractionComplex(float f, complex<float> cp){
+	complex<float> result;
+	complex<float> temp;
+	temp = (complex<float>)f;
+	result = temp - cp;
+	return result;
+}
--- src/Project_FFT/src/FFT.h
+++ src/Project_FFT/src/FFT.h
@@ -8,12 +8,24 @@
 #ifndef FFT_H_
 #define FFT_H_
 #include <iostream>
+#include <cmath>
+#include <vector>
+#include <complex>
+#include "BitReverse.h"
+#include "TwiddleFactor.h"
+
 using namespace std;
+
+#define N 1024
 
 class FFT {
 public:
 	FFT();
 	virtual ~FFT();
+	vector<complex<float>> calculFFT(vector<float> data_in);
+	complex<float> multiplicationComplex(float f, complex<float>);
+	complex<float> additionComplex(float f, complex<float>);
+	complex<float> soustractionComplex(float f, complex<float>);
 };
 
 #endif /* FFT_H_ */
